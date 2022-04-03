b5fd52f450be77e34bb575b96b22e687541ec59a
mabrur007 <mirmd.mabrur@gmail.com>
2022-04-03 13:05:33 +0600

    t-shirt showing

--- src/components/Tshirt/Tshirt.js
+++ src/components/Tshirt/Tshirt.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Tshirt.css'
 
 const Tshirt = (props) => {
     const { name, picture, price } = props.tShirt;
     return (
-        <div>
+        <div className='t-shirt'>
             <img src={picture} alt="product-img" />
             <h4>{name}</h4>
             <p>Price{ price}</p>
